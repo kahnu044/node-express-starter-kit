5a07631a593a91405b9413b41acf4dc30ed8f368
kahnu044 <kahnucharan044@gmail.com>
2024-08-13 13:14:54 +0530

    Server End Points Added

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
 // Server.js
+const express = require('express');
+const config = require('./config/config');
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+
+// Routes
+app.use('/', (req, res) => {
+    return res.status(200).json({
+        status: true,
+        message: "Welcome to the Backend server"
+    })
+});
+
+
+app.use('/api', (req, res) => {
+    return res.status(200).json({
+        status: true,
+        message: "Welcome to the Backend API server"
+    })
+});
+
+
+// Start the server
+app.listen(config.port, () => {
+    console.log(`Server running in ${config.env} mode on port ${config.port}`);
+});
